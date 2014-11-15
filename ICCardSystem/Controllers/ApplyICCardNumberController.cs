e46806254d8a1b5980b0eebf7289a33b11cdedba
zhangbojun <1009604388@qq.com>
2014-11-15 20:09:26 +0800

    重新修改ApplyICCardNumber

---
+++ ICCardSystem/Controllers/ApplyICCardNumberController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ICCardSystem.Models;
+using ICCardSystem.Models.VModels;
+using ICCardSystem.Utility;
+
+namespace ICCardSystem.Controllers
+{
+    public class ApplyICCardNumberController : Controller
+    {
+        private DALBase baseDAL = new DALBase(new ICModelContainer());
+        //
+        // GET: /ApplyICCardNumber/
+
+        public ActionResult ICCardNumber()
+        {
+            var djglList = baseDAL.Get<djgl>().ToList();
+            var bdIndex = baseDAL.Get<bhIndex>().ToList().Count;
+            ViewBag.djglList = djglList;
+            ViewBag.bdIndex = bdIndex;
+            return View();
+        }
+
+    }
+}
